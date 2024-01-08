d3c65f8752b152aa2e767b91b364d1e46890a58b
lilian <lilian.rajaonah@gmail.com>
2024-01-08 11:38:05 +0100

    2eme msg

--- rsa.py
+++ rsa.py
@@ -1,7 +1,7 @@
 # Programme Python pour afficher un message
 
 # Définir le message que vous souhaitez afficher
-message = "Bonjour ANIS, ceci est un message d'exemple!"
+message = "Bonjour Billel, ceci est un message d'exemple!"
 
 # Afficher le message à l'écran
 print(message)
